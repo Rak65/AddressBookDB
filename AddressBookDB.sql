fa1b302283fac457f0dbfaf351e4af508480e05b
Rak65 <rakeshp6505@gmail.com>
2023-06-10 00:29:32 +0530

    Ability to create a Address Book Table

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -1,3 +1,18 @@
 --UC1
 Create Database AddressBookDB;
 use AddressBookDB;
+
+--UC2
+Create table Address
+(
+FirstName varchar(20),
+lastName varchar(20),
+Address varchar(20),
+City varchar(10),
+State varchar(20),
+Zip varchar(6),
+PhoneNumber varchar(10),
+Email varchar(20)
+);
+
+select * from Address;
